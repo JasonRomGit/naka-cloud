208132bf076be04094e0d6eeb361e4d4d4c85965
Jason_Rom <345142162@qq.com>
2018-12-07 13:35:51 +0800

    init

---
+++ naka-common/src/main/java/com/naka/nakacommon/NakaCommonApplication.java
@@ -0,0 +1,12 @@
+package com.naka.nakacommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakaCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NakaCommonApplication.class, args);
+    }
+}
---
+++ naka-server/src/main/java/com/naka/nakaserver/NakaServerApplication.java
@@ -0,0 +1,12 @@
+package com.naka.nakaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NakaServerApplication.class, args);
+    }
+}
