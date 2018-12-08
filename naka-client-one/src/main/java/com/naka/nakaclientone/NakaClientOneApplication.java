b0eb000361ae7ab6c788bd48028144ed2fed8f31
Jason_Rom <345142162@qq.com>
2018-12-08 15:20:33 +0800

    first run microservices

--- naka-client-one/src/main/java/com/naka/nakaclientone/NakaClientOneApplication.java
+++ naka-client-one/src/main/java/com/naka/nakaclientone/NakaClientOneApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
+@ComponentScan(basePackages = {"com.naka.nakaclientone.controller",
+        "com.naka.nakaclientone.biz","com.naka.nakaclientone.dao"})
 public class NakaClientOneApplication {
 
     public static void main(String[] args) {
